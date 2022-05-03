c7b24c34c3db273cb90770a82571af20bb853035
zafri <zafrir1997@gmail.com>
2022-05-03 19:57:20 +0300

    commit with edit add and remove prepared

--- src/Task/Main.java
+++ src/Task/Main.java
@@ -1,8 +1,23 @@
 package Task;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args){
-        Task task=new Task();
-        System.out.println(task.get_Status_task());
+    public static void main(String[] args) throws ParseException {
+        List<Task> tasks=new ArrayList<>();
+        tasks.add(new Task("1","05.09.2022",Status.In_progress));
+        System.out.println(tasks.get(0).getExecution_date());
+        System.out.println(tasks.size());
+        tasks.get(0).setExecution_date("06.09.2022");
+        System.out.println(tasks.get(0).getExecution_date());
+        tasks.remove(tasks.get(0));
+        System.out.println(tasks.size());
+
     }
 }
--- src/Task/Task.java
+++ src/Task/Task.java
@@ -1,19 +1,29 @@
 package Task;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class Task {
-   private String description;
+    private String description;
     private String execution_date;
     private Status status_task;
     public Task(){
         this.status_task=Status.New;
     }
-    public Task(String description,String execution_date,Status status_task){
+    public Task(String description,String execution_date,Status status_task) throws ParseException {
         this.description=description;
-        this.execution_date=execution_date;
+        DateFormat df=new SimpleDateFormat("dd.MM.yyyy");
+        Date date=df.parse(execution_date);
+        this.execution_date=df.format(date);
         this.status_task=status_task;
     }
+
+
 
     public String getDescription() {
         return description;
@@ -28,7 +38,7 @@ public class Task {
     }
 
     public void setExecution_date(String execution_date) {
-        this.execution_date = execution_date;
+            this.execution_date = execution_date;
     }
 
     public Status get_Status_task() {
@@ -38,5 +48,7 @@ public class Task {
     public void set_Status_task(Status status_task) {
         this.status_task = status_task;
     }
+
+
 }
 
