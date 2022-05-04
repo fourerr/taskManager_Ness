1fa2983f4c0bcfb7d43683264f6885b6789e7a38
zafri <zafrir1997@gmail.com>
2022-05-04 10:55:01 +0300

    final -complete with example of edit , add and remove operation

--- src/Task/Main.java
+++ src/Task/Main.java
@@ -1,23 +1,60 @@
 package Task;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-public class Main {
-    public static void main(String[] args) throws ParseException {
-        List<Task> tasks=new ArrayList<>();
-        tasks.add(new Task("1","05.09.2022",Status.In_progress));
-        System.out.println(tasks.get(0).getExecution_date());
-        System.out.println(tasks.size());
-        tasks.get(0).setExecution_date("06.09.2022");
-        System.out.println(tasks.get(0).getExecution_date());
-        tasks.remove(tasks.get(0));
-        System.out.println(tasks.size());
 
+public class Main {
+
+    static List<Task> tasks=new ArrayList<>();// static list of tasks
+
+    /**
+     * main function
+     * @throws ParseException -can be error if parsing the execution date is failed
+     */
+    public static void main() throws ParseException {
+        /*נגדי
+          adding two tasks to list tasks
+         */
+        tasks.add(new Task("1","05/09/2022", Status.In_progress));
+        tasks.add(new Task("2","12/12/2022", Status.Suspended));
+
+
+        /*
+          print the sie of the list before delete
+         */
+        System.out.println("size of the list is "+tasks.size());
+
+
+        tasks.remove(getTaskByDescription("1"));// remove  element with description "1"
+
+
+        System.out.println("after delete one element in task list ,size of the list is "+tasks.size() +'\n');// print size of list after delete
+
+
+        /*
+            edit task with description 2 and change its status to Complete
+         */
+        Task t=getTaskByDescription("2");
+        t.set_Status_task(Status.Completed);
+
+        /*
+            iterate over the tasks list and print information about the task in list using toString function
+         */
+        for (Task task : tasks) {
+            System.out.println(task.toString());
+        }
+
+    }
+
+    /**
+     * get task object by its description using stream
+     * @param description the description of task -String
+     * @return task with the same description
+     */
+    public static Task getTaskByDescription(String description){
+
+       return(tasks.stream().filter(task->task.getDescription().equals(description)).findFirst().orElse(null));
     }
 }
--- src/Task/Task.java
+++ src/Task/Task.java
@@ -3,50 +3,88 @@ package Task;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.Locale;
 
-public class Task {
+/**
+ * class represent task properties
+ */
+public class Task  {
     private String description;
     private String execution_date;
     private Status status_task;
-    public Task(){
-        this.status_task=Status.New;
-    }
+
+    /**
+     * Ctor with 3 parameters
+     * @param description -description of the task-typical number task
+     * @param execution_date - date that the task supposed to execute
+     * @param status_task - the status of task including 4 options as describe in enum Status
+     * @throws ParseException - func can throw an error if the parsing fail (in particular execution_date field)
+     */
     public Task(String description,String execution_date,Status status_task) throws ParseException {
         this.description=description;
-        DateFormat df=new SimpleDateFormat("dd.MM.yyyy");
+        DateFormat df=new SimpleDateFormat("dd/MM/yyyy");
         Date date=df.parse(execution_date);
         this.execution_date=df.format(date);
         this.status_task=status_task;
     }
 
 
-
+    /**
+     * getter for description
+     * @return string description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * edit new description for specific task
+     * @param description string to change
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * getter for execution date
+     * @return string of execution_date
+     */
     public String getExecution_date() {
         return execution_date;
     }
 
+    /**
+     * edit execution date for specific task
+     * @param execution_date string to change
+     */
     public void setExecution_date(String execution_date) {
             this.execution_date = execution_date;
     }
 
+    /**
+     * getter for status task
+     * @return Status enum - status task
+     */
     public Status get_Status_task() {
         return status_task;
     }
 
+    /**
+     * edit status task for specific task
+     * @param status_task Status enum to change
+     */
     public void set_Status_task(Status status_task) {
         this.status_task = status_task;
+    }
+
+    /**
+     * function to print the task properties on the screen clearly
+     * @return string represent the task properties
+     */
+    public String toString(){
+        return "description of task: "+this.description+"\n"
+                +"execution date is: "+this.execution_date+"\n"
+                +"status of the task is: "+this.status_task.toString()+"\n";
     }
 
 
